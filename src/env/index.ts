91dd3207d4adc6a3db9b106942620a5f605dae67
Paulo Carreira <psalvescarreira@gmail.com>
2025-03-26 18:28:00 -0300

    Configure ESLint and run lint on files

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import {z} from 'zod'
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
@@ -8,9 +8,9 @@ const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env)
 
-if (_env.success === false){
-  console.error("❌ Invalid environment variables", _env.error.format())
-  throw new Error("Invalid environment variables")
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables', _env.error.format())
+  throw new Error('Invalid environment variables')
 }
 
 export const env = _env.data
