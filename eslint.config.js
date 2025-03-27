b18d2865af473d655c12a74f0dfd21aaa69dba28
Paulo Carreira <psalvescarreira@gmail.com>
2025-03-26 22:54:15 -0300

    Add password hashing and email verification to register controller

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,14 @@
 'use strict'
+const { defineConfig } = require('eslint/config')
 
-module.exports = require('neostandard')({
-  ts: true,
-  ignores: ['build/**/*', 'node_modules/**/*']
-})
+module.exports = defineConfig([
+  require('neostandard')({
+    ts: true,
+    ignores: ['build/**/*', 'node_modules/**/*'],
+  }),
+  {
+    rules: {
+      camelcase: 'off',
+    },
+  },
+])
