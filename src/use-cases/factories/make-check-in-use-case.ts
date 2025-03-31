805c92cf88b1596365c957389538afd90ae5882b
Paulo Carreira <psalvescarreira@gmail.com>
2025-03-31 16:43:30 -0300

    Implement factories for each use case

---
+++ src/use-cases/factories/make-check-in-use-case.ts
@@ -0,0 +1,11 @@
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-checkins-repository'
+import { CheckinUseCase } from '../check-in'
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
+
+export function makeCheckInUseCase () {
+  const checkInsRepository = new PrismaCheckInsRepository()
+  const gymsRepository = new PrismaGymsRepository()
+  const useCase = new CheckinUseCase(checkInsRepository, gymsRepository)
+
+  return useCase
+}
