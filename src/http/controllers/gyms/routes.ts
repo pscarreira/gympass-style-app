9de28ea1beef2ab84fdf398d69930561166d575e
Paulo Carreira <psalvescarreira@gmail.com>
2025-04-01 17:41:06 -0300

    Gyms Search/Nearby Controller

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,13 @@
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
+import { create } from './create.controller'
+import { nearby } from './nearby.controller'
+import { search } from './search.controller'
 
 export async function gymRoutes (app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.post('/gyms', create)
+  app.get('/gyms/nearby', nearby)
+  app.get('/gyms/search', search)
 }
---
+++ src/http/controllers/gyms/search.controller.ts
@@ -0,0 +1,24 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export async function search (request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).optional()
+  })
+
+  const {
+    query,
+    page
+  } = searchGymsQuerySchema.parse(request.body)
+
+  const searchGymUseCase = makeSearchGymsUseCase()
+  const { gyms } = await searchGymUseCase.execute({
+    query, page
+  })
+
+  return reply.status(200).send({
+    gyms
+  })
+}
